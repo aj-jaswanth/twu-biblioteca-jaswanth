e80282b7a21d133e3a002ad0f673019fc77d66da
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-06-08 08:31:59 +0530

    Added specifations for Biblioteca

--- src/com/twu/biblioteca/Biblioteca.java
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -2,7 +2,31 @@ package com.twu.biblioteca;
 
 public class Biblioteca {
 
-    public static void main(String[] args) {
-        System.out.println("Bengaluru Public Library Welcomes you!");
+    private MainMenu mainMenu = new MainMenu(new String[]{"List Books", "Quit"});
+    private Library library = new Library();
+
+    public void start() {
+        welcomeUser();
+        while (true) {
+            if (getOptionFromUser() == 1)
+                library.displayAvailableBooks();
+            else
+                return;
+        }
+    }
+
+    public void welcomeUser() {
+        System.out.println("Bengaluru public library welcomes you!");
+    }
+
+    public int getOptionFromUser() {
+        int selectedOption;
+        while (true) {
+            mainMenu.displayOptions();
+            selectedOption = mainMenu.selectedOption();
+            if (selectedOption == -1)
+                continue;
+            return selectedOption;
+        }
     }
 }
--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -1,6 +1,77 @@
 package com.twu.biblioteca;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class BibliotecaTest {
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @Test
+    public void shouldPrintWelcomeMessage() {
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.welcomeUser();
+
+        String actualWelcomeMessage = outputStream.toString();
+
+        assertThat(actualWelcomeMessage, is(equalTo("Bengaluru public library welcomes you!\n")));
+    }
+
+    @Test
+    public void shouldReturnUserSelectedOption() {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("3\n2\n".getBytes());
+        System.setIn(inputStream);
+        Biblioteca biblioteca = new Biblioteca();
+
+        int actualSelectedOption = biblioteca.getOptionFromUser();
+
+        assertThat(actualSelectedOption, is(equalTo(2)));
+    }
+
+    @Test
+    public void shouldStartBibliotecaAndDisplayBooksIfSelected() {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("1\n2\n".getBytes());
+        System.setIn(inputStream);
+        Biblioteca biblioteca = new Biblioteca();
+
+        biblioteca.start();
+        String actualOutput = outputStream.toString();
+
+        assertThat(actualOutput, is(equalTo("Bengaluru public library welcomes you!\n1. List Books\n2. Quit" +
+                "\nSelect an option : 1 Algorithms Cormen 2014\n" +
+                "2 Physics Michio 2009\n3 C Dennis 1982\n1. List Books\n2. Quit\nSelect an option : ")));
+    }
+
+    @Test
+    public void shouldStartBibliotecaAndQuitIfSelected() {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("2".getBytes());
+        System.setIn(inputStream);
+        Biblioteca biblioteca = new Biblioteca();
+
+        biblioteca.start();
+        String actualOutput = outputStream.toString();
+
+        assertThat(actualOutput, is(equalTo("Bengaluru public library welcomes you!\n" +
+                "1. List Books\n2. Quit\nSelect an option : ")));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+        System.setIn(System.in);
+    }
 }
