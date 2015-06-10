9f1184fd78ed1e107a257ecab94cd39388443d36
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-06-10 07:01:09 +0530

    Added specification to checkout a book from library

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -5,12 +5,7 @@ import java.util.ArrayList;
 public class Library {
 
     private ArrayList<Book> availableBooks = new ArrayList<Book>();
-
-    public Library() {
-        availableBooks.add(new Book("Algorithms", "Cormen", 2014));
-        availableBooks.add(new Book("Physics", "Michio", 2009));
-        availableBooks.add(new Book("C", "Dennis", 1982));
-    }
+    private ArrayList<Book> checkedOutBooks = new ArrayList<Book>();
 
     public void addBook(Book book) {
         availableBooks.add(book);
@@ -19,5 +14,12 @@ public class Library {
     public void displayAvailableBooks() {
         for (int x = 0; x < availableBooks.size(); x++)
             System.out.println((x + 1) + " " + availableBooks.get(x));
+    }
+
+    public void checkout(int bookIndex) {
+        if (0 < bookIndex && bookIndex <= availableBooks.size()) {
+            checkedOutBooks.add(availableBooks.get(bookIndex - 1));
+            availableBooks.remove(bookIndex - 1);
+        }
     }
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -18,17 +18,31 @@ public class LibraryTest {
     }
 
     @Test
-    public void addingANewBookToLibraryShouldDisplayItInAvailableListR() {
+    public void addingANewBookToLibraryShouldDisplayItInAvailableList() {
         Library library = new Library();
-
+        library.addBook(new Book("Algorithms", "Cormen", 2014));
+        library.addBook(new Book("Physics", "Michio", 2009));
+        library.addBook(new Book("C", "Dennis", 1982));
         library.addBook(new Book("Refactoring", "Martin", 2005));
+
         library.displayAvailableBooks();
         String actualBooksList = outputStream.toString();
 
-        assertEquals(actualBooksList, "1 Algorithms Cormen 2014\n2 Physics Michio 2009\n" +
-                "3 C Dennis 1982\n4 Refactoring Martin 2005\n");
+        assertEquals("1 Algorithms Cormen 2014\n2 Physics Michio 2009\n" +
+                "3 C Dennis 1982\n4 Refactoring Martin 2005\n", actualBooksList);
     }
 
+    @Test
+    public void checkedOutBookShouldNotBeAvailableInLibrary() {
+        Library library = new Library();
+        library.addBook(new Book("Algorithms", "Cormen", 2014));
+
+        library.checkout(1);
+        library.displayAvailableBooks();
+        String actualBooksList = outputStream.toString();
+
+        assertEquals("", actualBooksList);
+    }
 
     @After
     public void tearDown() {
